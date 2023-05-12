0de0058aca9518bc750686a3d3ed6f8d90741929
sonnysam <samuelagbenyo067@gmail.com>
2023-05-12 18:49:49 +0000

    project initialized

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+const Colors = {
+    light: "#eeee",
+    grey: "#D8D8D8",
+    darkGray: "#647E68",
+    primary: "rgb(96, 176, 244)",
+    secondary: "#F26C68",
+    dark: "#000",
+    white: "#ffff",
+    green: "#4f9d72",
+    lightGreen: "#A8E890",
+  };
+  
+  export default Colors;
+  
