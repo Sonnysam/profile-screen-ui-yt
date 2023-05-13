ad5bf9ddd75ab9411e16408c39f8b729640e7e35
sonnysam <samuelagbenyo067@gmail.com>
2023-05-13 16:17:34 +0000

    colors modified and top component complete

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import Top from "./components/Top";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      style={styles.backgroundImage}
+      source={require("./assets/bg.png")}
+      // blurRadius={1}
+    >
+      <View style={styles.container}>
+        
+        {/* TOP SECTION */}
+        <Top />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    resizeMode: "cover",
+  },
+  container: {
+    marginHorizontal: 10,
+    marginTop: 30,
   },
 });
