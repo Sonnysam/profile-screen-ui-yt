d588356ab702713a4e4630e54bcc3a4a59d16b4a
sonnysam <samuelagbenyo067@gmail.com>
2023-05-13 17:54:23 +0000

    styles changed

--- components/Bottom.js
+++ components/Bottom.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
   bottomSection: {
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 15,
+    marginTop: 20,
   },
   bottomSectionText: {
     fontWeight: "bold",
